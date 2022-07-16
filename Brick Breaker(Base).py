8c7f3a2095147b668f75b4cc47f2237674c7c484
lobeugeul548 <92210086+lobeugeul548@users.noreply.github.com>
2022-07-16 17:39:01 +0900

    벽돌 추가

--- Brick Breaker(Base).py
+++ Brick Breaker(Base).py
@@ -3,30 +3,42 @@ import sys
 import random
 
 
-# 사각형 클래스 만들기
-class Rectangle:
+# 벽돌 클래스 만들기
+class Brick:
     def __init__(self, x, y):
         self.start_x = x
         self.start_y = y
-        self.x = 234
-        self.y = 105
-        self.color = random.choice(color_list)
+        self.x = 234  #가로 5개
+        self.y = 105  #세로 4개
+        self.color = random.randint(50, 255), random.randint(50, 255), random.randint(50, 255)
 
     def draw_rectangle(self):
         pygame.draw.rect(screen, self.color, [self.start_x, self.start_y, self.x, self.y])
 
 
-# 바 만들기
+# 바 클래스 만들기
 class Bar:
     def __init__(self):
         self.start_x = 450
-        self.start_y = 885
+        self.start_y = 870
         self.x = 300
-        self.y = 15
-        self.color = random.choice(color_list)
+        self.y = 25
+        self.speed = 2
+        self.color = random.randint(50, 255), random.randint(50, 255), random.randint(50, 255)
 
     def draw_bar(self):
         pygame.draw.rect(screen, self.color, [self.start_x, self.start_y, self.x, self.y])
+
+
+# 공 클래스 만들기
+class Ball:
+    def __init__(self):
+        self.pos = [600, 870]
+        self.radius = 15
+        self.color = random.randint(50, 255), random.randint(50, 255), random.randint(50, 255)
+
+    def draw_ball(self):
+        pygame.draw.circle(screen, self.color, self.pos, self.radius)
 
 
 pygame.init()  # pygame 초기화
@@ -42,7 +54,7 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 ORANGE = (255, 127, 0)
 BROWN = (163, 111, 64)
-color_list = [PINK, PURPLE, GREEN, RED, BLUE, YELLOW, ORANGE, BROWN]
+color_list = [PINK, PURPLE, GREEN, RED, BLUE, YELLOW, ORANGE, BROWN, BLACK]
 
 # pygame 창 설정
 size = (1200, 900)
@@ -53,12 +65,42 @@ Quit = False
 clock.tick(60)
 
 # 메인 루프
+bar = Bar()
+brick_list = []
+for i in range(5):
+    for j in range(4):
+        brick = Brick(5 + 239 * i, 5 + 110 * j)
+        brick_list.append(brick)
+left_move = False
+right_move = False
+
 while not Quit:
     for event in pygame.event.get():  # 이벤트 처리 루프
         if event.type == pygame.QUIT:  # 종료 버튼(X)를 눌렀을 때 창 닫기
             Quit = True
-        screen.fill(WHITE)
-        pygame.display.flip()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                left_move = True
+            if event.key == pygame.K_RIGHT:
+                right_move = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                left_move = False
+            if event.key == pygame.K_RIGHT:
+                right_move = False
+    if left_move:
+        bar.start_x -= bar.speed
+    elif right_move:
+        bar.start_x += bar.speed
+    if bar.start_x < 0:
+        bar.start_x = 0
+    elif bar.start_x > 900:
+        bar.start_x = 900
+    screen.fill(BLACK)
+    bar.draw_bar()
+    for i in range(20):
+        brick_list[i].draw_rectangle()
+    pygame.display.flip()
 
 # pygame, 밑 system 종료
 pygame.quit()
